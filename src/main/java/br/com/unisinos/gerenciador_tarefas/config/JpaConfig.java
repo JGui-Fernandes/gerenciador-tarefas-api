94f636e6aad9a07e1ceb7ad1a7fc50a7d5df5e6e
João Fernandes <jguiborges@gmail.com>
2026-05-28 20:53:11 -0300

    Criando entidade Task

---
+++ src/main/java/br/com/unisinos/gerenciador_tarefas/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package br.com.unisinos.gerenciador_tarefas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
